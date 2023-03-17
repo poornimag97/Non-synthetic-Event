28c86d2ed74f262a3b7a09c423a3825c680ac850
poornimag97 <126610347+poornimag97@users.noreply.github.com>
2023-03-17 18:25:45 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,28 @@
+import React,{Component} from "react";
+export default class App extends Component{
+  constructor()
+  {
+    super()
+    this.state={
+      username:""
+    } 
+  }
+ componentDidMount()
+    {
+      let input=document.querySelector('input')
+     input.addEventListener("keypress",(e)=>{
+        this.setState({username:e.target.value})
+      }
+      )
+    }
+    render(){
+
+   
+    return(
+      <>
+      <div>{this.state.username} </div>
+      <input type="text" />
+      </>
+    )
+    }
+  }
